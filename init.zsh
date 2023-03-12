9daa0edb41a8e0cf25d0ee29e2b191142d8f9512
Joke de Buhr <joke@xckk.de>
2023-03-12 16:22:32 +0100

    fix: new version detection message
    Signed-off-by: Joke de Buhr <joke@xckk.de>

--- init.zsh
+++ init.zsh
@@ -6,6 +6,6 @@
   local compfile=$1/functions/_steampipe
   if [[ ! -e $compfile || $compfile -ot $command ]]; then
     $command completion zsh >| $compfile
-    zimfw check-dumpfile
+    print -u2 -PR "* Detected a new version 'steampipe'. Regenerated completions."
   fi
 } ${0:h}
